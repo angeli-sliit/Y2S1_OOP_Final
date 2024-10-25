77e23096b83de0eaa9d56aebedf6c2a0e9a38621
angeli-sliit <159871522+angeli-sliit@users.noreply.github.com>
2024-10-26 04:08:11 +0530

    Add files via upload

---
+++ June2022/Q2A/DemoApp.java
@@ -0,0 +1,44 @@
+package Q2A;
+
+import java.util.ArrayList;
+
+public class DemoApp{
+	public static void main(String[] args) {
+		TCalculation<Integer> obj1 = new TCalculation<>();
+		TCalculation<Double> obj2 = new TCalculation<>();
+		
+		obj1.append(10);
+		obj1.append(20);
+		obj1.append(30);
+		
+		obj2.append(10.4);
+		obj2.append(20.2);
+		obj2.append(30.3);
+		
+		System.out.println("Average of Integer ArrayList : " + obj1.avarage());
+		System.out.println("Average of Double ArrayList : " + obj2.avarage());
+	}
+}
+
+
+
+class TCalculation <T extends Number>{
+	ArrayList<T> numList = new ArrayList<>();
+	
+	
+	public T append(T num) {
+		numList.add(num);
+		return null;
+	}
+	
+	public Double avarage() {
+		Double avg = 0.0;
+		Double tot =0.0;
+		for(T i: numList) {
+			tot=tot+i.doubleValue();
+		}
+		avg=tot/numList.size();
+		return  avg;
+	}
+}
+
---
+++ June2022/Q4/Car.java
@@ -0,0 +1,6 @@
+package Q4;
+
+public abstract class Car {
+	public abstract void assembleLight();
+	public abstract void assembleMotionSensor();
+}
