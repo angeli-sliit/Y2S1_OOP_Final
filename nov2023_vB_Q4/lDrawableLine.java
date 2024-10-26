723adf03a099ab802d7710626b887804b7adb2ca
angeli-sliit <159871522+angeli-sliit@users.noreply.github.com>
2024-10-26 15:04:48 +0530

    Delete nov2023_vB_Q4 directory

--- nov2023_vB_Q4/lDrawableLine.java
+++
@@ -1,5 +0,0 @@
-package nov2023_vB_Q4;
-
-public interface lDrawableLine {
-	public void drawLines(String robotType);
-}
--- nov2023_vB_Q4/lDrawableshapes.java
+++
@@ -1,5 +0,0 @@
-package nov2023_vB_Q4;
-
-public interface lDrawableshapes {
-	public void drawshapes(String robotType);
-}
