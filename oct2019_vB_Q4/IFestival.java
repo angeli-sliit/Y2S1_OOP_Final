5933c58a21399410ea85052e8de0fa6d491cd696
angeli-sliit <159871522+angeli-sliit@users.noreply.github.com>
2024-10-26 15:04:32 +0530

    Delete oct2019_vB_Q4 directory

--- oct2019_vB_Q4/IFestival.java
+++
@@ -1,6 +0,0 @@
-package oct2019_vB_Q4;
-
-public interface IFestival {
-	void performEvent();
-	double getBudget();
-}
--- oct2019_vB_Q4/IPrograms.java
+++
@@ -1,7 +0,0 @@
-package oct2019_vB_Q4;
-
-public interface IPrograms {
-	
-	 void offerPrograms();
-	 double getCost();
-}
