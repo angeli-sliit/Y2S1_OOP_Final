db8c704a84967c4790ebaa1b2f6deefaea60053f
angeli-sliit <159871522+angeli-sliit@users.noreply.github.com>
2024-10-26 15:05:02 +0530

    Delete nov2022_vB_Q4 directory

--- nov2022_vB_Q4/IPrepareDeliciously.java
+++
@@ -1,6 +0,0 @@
-package nov2022_vB_Q4;
-
-public interface IPrepareDeliciously {
-	void addFlavour();
-	double getcost();
-}
--- nov2022_vB_Q4/IPrepareQuickly.java
+++
@@ -1,5 +0,0 @@
-package nov2022_vB_Q4;
-
-public interface IPrepareQuickly {
-	void deliveryTime(); 
-}
