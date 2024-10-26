84f5ce2d9ead0a2c28644d9fe6824baab0b944e2
angeli-sliit <159871522+angeli-sliit@users.noreply.github.com>
2024-10-26 15:02:04 +0530

    Delete June2023_vB_Q4 directory

--- June2023_vB_Q4/MissileOperation.java
+++
@@ -1,6 +0,0 @@
-package June2023_vB_Q4;
-
-public interface MissileOperation {
-	public void initiateoperation(String location);   
-	
-}
--- June2023_vB_Q4/MissileSystem.java
+++
@@ -1,7 +0,0 @@
-package June2023_vB_Q4;
-
- public interface MissileSystem {
-	
-	public void launch(String source);
-	public void blast(String destination);
-}
